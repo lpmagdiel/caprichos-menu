a50faff743c7c22574c58e1c459b4799ed8687dc
Magdiel Lopez <lopmag.lopez@gmail.com>
2024-03-12 21:23:19 +0100

    drinks

--- caprichos/src/App.svelte
+++ caprichos/src/App.svelte
@@ -1,47 +1,47 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+    import CategoryBox from "./lib/CategoryBox.svelte";
+    import foods from "../public/foods.json";
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <div class="container-menu-text">
+        <h1>Menú</h1>
+    </div>
+    <header>
+        <h2>Caprichos café</h2>
+    </header>
+    <CategoryBox title="Comida ligera" foods={foods.light}/>
+    <CategoryBox title="Comida a la carta" foods={foods.saucers}/>
+    <CategoryBox title="Café helado" foods={foods.couldCoffe}/>
+    <CategoryBox title="Café helado" foods={foods.drinks}/>
 </main>
 
+
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+    main{
+        width: 100%;
+        min-height: 100vh;
+        display: inline-block;
+    }
+    .container-menu-text{
+        position: absolute;
+        font-size: 3rem;
+    }
+    .container-menu-text h1{
+        writing-mode: sideways-lr;
+        text-orientation: upright;
+        margin-left: -65px;
+        font-family: "PTSerif";
+        font-weight: bold;
+        color: #FD5211;
+    }
+    main h2{
+        text-align: right;
+        padding: 10px;
+        font-family: "PTSerif";
+        text-decoration: underline;
+        background-color: #FD5211;
+        float: right;
+        color: #ffffff;
+    }
 </style>
---
+++ caprichos/src/lib/CategoryBox.svelte
@@ -0,0 +1,68 @@
+<script>
+    export let title = "";
+    export let foods = [];
+</script>
+
+<div class="container">
+    <div class="title-bar">
+        <h2>{title}</h2>
+    </div>
+    <div class="table-food">
+        {#each foods as food}
+                <div>
+                    <b>{food.name}</b>
+                    <p>{food.description}</p>
+                </div>
+                <div class="line">
+                    <h3>C$ {food.price}</h3>
+                </div>
+        {/each}
+        </div>
+</div>
+
+<style>
+    .container {
+        box-sizing: border-box;
+        display: flex;
+        width: 100%;
+        padding-top: 50px;
+        padding-left: 60px;
+        position: relative;
+        flex-direction: column;
+    }
+    .title-bar {
+        height: 50px;
+        width: 100%;
+    }
+    .title-bar::before {
+        content: "";
+        position: absolute;
+        width: 60%;
+        background-color: #fbcea3;
+        height: 20px;
+        margin-top: 17px;
+    }
+    .title-bar h2 {
+        position: relative;
+        z-index: 2;
+        padding-left: 4px;
+    }
+    .line {
+        border-left: 4px solid #fd5211;
+        padding-left: 4px;
+    }
+    .table-food{
+        display: grid;
+        grid-template-columns: 1fr 100px;
+        font-size: 0.8rem;
+    }
+    .table-food > div{
+        padding: 5px;
+        margin: 8px 0px;
+        display: grid;
+        align-content: center;
+    }
+    .table-food > div > p{
+        color: #686868;
+    }
+</style>
